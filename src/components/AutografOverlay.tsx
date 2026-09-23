b02a62080da2a3234e38b675abdc5da542c5ec37
aleksandrdj <aleksandrsavenkov9@gmail.com>
2026-09-23 18:50:07 +0300

    Update AutografOverlay.tsx

--- src/components/AutografOverlay.tsx
+++ src/components/AutografOverlay.tsx
@@ -4,10 +4,10 @@ import { AUTOGRAF_PATHS, AUTOGRAF_VIEWBOX } from '../data/autografData';
 // Traced against the supplied 595 x 345 artwork, not the ArtDeejay wordmark.
 // The guides are masks only: the original filled paths always define the ink.
 const strokes = [
-  { d: 'M 13 298 L 317 45 Q 326 38 320 50 L 184 294', width: 11, start: 0, end: .34 },
-  { d: 'M 184 294 L 350 122 Q 357 116 351 128 L 283 220 Q 277 228 286 221 L 344 175 Q 350 170 341 183 L 268 278 Q 263 284 272 278 L 430 125', width: 11, start: .34, end: .65 },
-  { d: 'M 430 125 Q 334 137 157 213', width: 11, start: .65, end: .77 },
-  { d: 'M 100 292 Q 320 237 579 150 Q 589 146 577 146 L 439 147 Q 425 146 421 164', width: 11, start: .77, end: 1 },
+  { d: 'M 13 298 L 317 45 Q 326 38 320 50 L 184 294', width: 6, start: 0, end: .34 },
+  { d: 'M 184 294 L 350 122 Q 357 116 351 128 L 283 220 Q 277 228 286 221 L 344 175 Q 350 170 341 183 L 268 278 Q 263 284 272 278 L 430 125', width: 6, start: .34, end: .65 },
+  { d: 'M 430 125 Q 334 137 157 213', width: 6, start: .65, end: .77 },
+  { d: 'M 100 292 Q 320 237 579 150 Q 589 146 577 146 L 439 147 Q 425 146 421 164', width: 6, start: .77, end: 1 },
 ];
 interface Props {
   scrollProgressRef: React.MutableRefObject<number>;
